c0c10c0a2bae74ac49f77e254632fbd12ee0b2e4
Greg Boyington <greg@automagick.us>
2014-07-23 12:03:11 -0400

    fixed dotted name of crontab module in example project

--- example-project/project/walter/admin.py
+++ example-project/project/walter/admin.py
@@ -4,10 +4,10 @@ from cronkite.models import Job
 # Option 1:
 # create jobs for any new functions in project.crontab, but do not
 # delete missing ones or update existing ones.
-Job.auto_discover('project.crontab', delete_first=False)
+Job.auto_discover('project.walter.crontab', delete_first=False)
 
 # Option 2:
 # Purge all jobs created for functions in project.crontab, then
 # create new ones for each function we find therein.
 # WARNING: This will discard any changes made in the admin UI!
-#Job.auto_discover('project.crontab', delete_first=True)
+#Job.auto_discover('project.walter.crontab', delete_first=True)
